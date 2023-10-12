05f56583f4d483cabef1df32cc5fec9de67bd9c3
Daniel Rosel <daniel@alves.world>
2023-10-12 22:49:31 +0200

    improved and more complete script

--- script.sh
+++ script.sh
@@ -1,64 +1,94 @@
 #!/bin/sh
 
-echo "Welcome to the beginner-friendly Git assistant!"
-echo "Please tell me in simple terms what you'd like to do with Git."
+function display_help() {
+    gum style --background 255 --foreground 27 --align center --width 60 --border rounded \
+        "Available Commands" \
+        "'start a new project' - Initializes a new Git repository." \
+        "'save my changes' - Adds and commits changes." \
+        "'get latest updates' - Pulls the latest changes." \
+        "'share my updates' - Pushes committed changes." \
+        "'show my changes' - Shows the repository status." \
+        "'create a branch' - Creates a new branch." \
+        "'switch branch' - Switches to another branch." \
+        "'show history' - Displays the commit history." \
+        "'help' - Displays this help information." \
+        "'exit' or 'quit' - Exits the script."
+}
+
+echo "Welcome to the enhanced Git assistant!"
+gum spin --spinner globe -- sleep 2
 
 # Loop forever until user quits
 while true; do
     echo ""
-    read -p "Git Command: " cmd
+    cmd=$(gum input --placeholder "Enter a Git Command (or 'help' for guidance):")
 
     case "$cmd" in
         "exit" | "quit")
+            gum spin --spinner moon -- sleep 1
             echo "Goodbye!"
             break
             ;;
-            
-        "start a new project" | "initialize repository" | "create a new project")
-            DIRNAME=$(gum input --placeholder "Enter the directory name for your new project (leave empty for current directory):")
+
+        "help")
+            display_help
+            ;;
+
+        "start a new project")
+            DIRNAME=$(gum input --placeholder "Directory name (empty for current directory):")
             if [ -z "$DIRNAME" ]; then
                 git init
             else
                 mkdir "$DIRNAME" && cd "$DIRNAME" && git init
             fi
+            gum spin --spinner dot -- sleep 2
             ;;
-            
-        "save my changes" | "commit my changes")
+
+        "save my changes")
             git add -A
-            MSG=$(gum input --placeholder "Enter a message describing your changes:")
+            MSG=$(gum input --placeholder "Describe your changes:")
             git commit -m "$MSG"
+            gum spin --spinner pulse -- sleep 2
             ;;
-            
-        "get latest updates" | "pull updates")
-            git pull
+
+        "get latest updates")
+            gum spin --spinner jump -- git pull
             ;;
-            
-        "share my updates" | "push my updates")
-            git push
+
+        "share my updates")
+            gum spin --spinner pulse -- git push
             ;;
-            
-        "show my changes" | "list changes")
-            git status
+
+        "show my changes")
+            STATUS=$(git status)
+            gum pager <<<$STATUS
             ;;
-            
-        "create a branch" | "make a new branch")
-            BRANCH_NAME=$(gum input --placeholder "Enter a name for your new branch:")
+
+        "create a branch")
+            BRANCH_NAME=$(gum input --placeholder "Branch name:")
             git checkout -b "$BRANCH_NAME"
+            gum spin --spinner meter -- sleep 2
             ;;
-            
-        "switch branch" | "change branch")
+
+        "switch branch")
             BRANCHES=$(git branch | sed 's/* //g')
-            TARGET_BRANCH=$(echo "$BRANCHES" | gum filter --placeholder "Choose a branch:")
+            TARGET_BRANCH=$(echo "$BRANCHES" | gum choose)
             git checkout "$TARGET_BRANCH"
+            gum spin --spinner hamburger -- sleep 2
             ;;
-            
-        "show history" | "list commits")
-            git log --oneline | less
+
+        "show history")
+            LOGS=$(git log --oneline)
+            gum pager <<<$LOGS
             ;;
 
         *)
-            echo "I'm sorry, I'm not sure how to do that. Try another command or use 'exit' to quit."
+            gum style --foreground 255 --background 124 --align center --width 50 --border rounded \
+                "Unknown Command!" \
+                "-------------------" \
+                "Try 'help' for available commands."
             ;;
+
     esac
 done
 
